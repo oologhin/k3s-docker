817b2262c4d1c700c694cace43eee709fdb5a383
Diezus.net <Diezus.net@gmail.com>
2024-06-17 00:16:08 +0200

    fixed deploy- no systemd

--- start_services.sh
+++ start_services.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-
-
+# Start Docker
+service docker start
 
 # Install K3s if not already installed
 if [ ! -f /usr/local/bin/k3s ]; then
   curl -sfL https://get.k3s.io | sh -
-  systemctl enable k3s
 fi
 
-
-# Wait for systemd to initialize
-sleep 5
-
-# Start K3s
-systemctl start k3s
+# Start K3s server
+/usr/local/bin/k3s server &
 
 # Start SSH service
-systemctl start ssh
+service ssh start
+
+# Keep the container running
+tail -f /dev/null
